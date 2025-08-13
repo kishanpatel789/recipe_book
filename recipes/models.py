826bca1d2fd34bcd7d74c692507ff28b32e6cb22
Kishan Patel <kishanpatel789@gmail.com>
2025-08-13 11:59:55 -0500

    Create StepIngredient model

--- recipes/models.py
+++ recipes/models.py
@@ -2,30 +2,6 @@ from django.db import models
 from django.utils.text import slugify
 
 
-class Tag(models.Model):
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def save(self, *args, **kwargs):
-        base_slug = slugify(self.slug)
-        slug = base_slug
-        counter = 1
-
-        # Ensure uniqueness of the slug
-        while Tag.objects.filter(slug=slug).exists():
-            slug = f"{base_slug}-{counter}"
-            counter += 1
-
-        self.slug = slug
-
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.slug
-
-    class Meta:
-        ordering = ["slug"]
-
-
 class Recipe(models.Model):
     name = models.CharField(unique=True, max_length=200)
     slug = models.SlugField(unique=True, max_length=200, blank=True)
@@ -33,7 +9,8 @@ class Recipe(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     # created_by =
     # modified_by =
-    tags = models.ManyToManyField(Tag)
+
+    tags = models.ManyToManyField("Tag")
     complementary = models.ManyToManyField("self", symmetrical=False)
 
     def save(self, *args, **kwargs):
@@ -62,12 +39,23 @@ class Step(models.Model):
     recipe = models.ForeignKey(Recipe, related_name="steps", on_delete=models.CASCADE)
     order_id = models.SmallIntegerField()
     instruction = models.CharField(max_length=256)
+    ingredients = models.ManyToManyField("Ingredient", through="StepIngredient")
 
     def __str__(self):
-        return f"{self.recipe.name} {self.order_id}"
+        return f"'{self.recipe.name}'/{self.order_id}"
 
     class Meta:
         ordering = ["order_id"]
+
+
+class Ingredient(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        ordering = ["name"]
 
 
 class Unit(models.Model):
@@ -83,15 +71,33 @@ class Unit(models.Model):
         ordering = ["name"]
 
 
-class Ingredient(models.Model):
-    step = models.ForeignKey(Step, related_name="ingredients", on_delete=models.CASCADE)
+class StepIngredient(models.Model):
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     order_id = models.SmallIntegerField()
     quantity = models.FloatField(null=True)
     unit = models.ForeignKey(Unit, null=True, on_delete=models.SET_NULL)
-    item = models.CharField(max_length=200)
+
+
+class Tag(models.Model):
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def save(self, *args, **kwargs):
+        base_slug = slugify(self.slug)
+        slug = base_slug
+        counter = 1
+
+        # Ensure uniqueness of the slug
+        while Tag.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+
+        self.slug = slug
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.item}"
+        return self.slug
 
     class Meta:
-        ordering = ["order_id"]
+        ordering = ["slug"]
