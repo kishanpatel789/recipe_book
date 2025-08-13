a0d03215a5f36a7f991227c77586de8490da1f79
Kishan Patel <kishanpatel789@gmail.com>
2025-08-13 08:53:19 -0500

    Update Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,22 +1,38 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Recipe(models.Model):
     name = models.CharField(unique=True, max_length=200)
     slug = models.SlugField(unique=True, max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True)
     # created_by =
     # modified_by =
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            # Ensure uniqueness of the slug
+            while Tag.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.slug
+        return self.name
 
     class Meta:
         ordering = ["name"]
 
 
-class Direction(models.Model): ...
+class Step(models.Model): ...
 
 
 class Ingredient(models.Model): ...
@@ -26,6 +42,3 @@ class Tag(models.Model): ...
 
 
 class Unit(models.Model): ...
-
-
-class User(models.Model): ...
