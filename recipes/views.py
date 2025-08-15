9f19f8e232701efb768189be54fa259947644e0e
Kishan Patel <kishanpatel789@gmail.com>
2025-08-15 10:58:06 -0500

    Start recipe list view

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Recipe
 
 
 def index(request):
     return HttpResponse("Recipe Book is live!")
+
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+
+    return render(request, "recipes/recipe/list.html", {"recipes": recipes})
+
+
+def recipe_detail(request): ...
+
+
+def recipe_create(request): ...
+
+
+def recipe_edit(request): ...
