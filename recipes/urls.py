e18be652f85f2fe0539aecf62f02a5abd15ffa91
Kishan Patel <kishanpatel789@gmail.com>
2025-08-15 11:16:02 -0500

    Start recipe detail with steps

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("recipes/", views.recipe_list, name="recipe_list"),
+    path("recipes/<str:recipe_slug>/", views.recipe_detail, name="recipe_detail"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Recipe
 
@@ -14,7 +14,10 @@ def recipe_list(request):
     return render(request, "recipes/recipe/list.html", {"recipes": recipes})
 
 
-def recipe_detail(request): ...
+def recipe_detail(request, recipe_slug):
+    recipe = get_object_or_404(Recipe, slug=recipe_slug)
+
+    return render(request, "recipes/recipe/detail.html", {"recipe": recipe})
 
 
 def recipe_create(request): ...
