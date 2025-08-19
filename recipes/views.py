98da96896c4b4ef90e10d2fb2ef03431993cd49d
Kishan Patel <kishanpatel789@gmail.com>
2025-08-19 09:39:23 -0500

    Aggregate ingredients for recipe detail

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
+from django.db.models import F, Min, Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from .models import Recipe
+from .models import Recipe, StepIngredient
 
 
 def index(request):
@@ -16,8 +17,27 @@ def recipe_list(request):
 
 def recipe_detail(request, recipe_slug):
     recipe = get_object_or_404(Recipe, slug=recipe_slug)
+    ingredients = (
+        StepIngredient.objects.filter(step__recipe__slug=recipe_slug)
+        .values(
+            "ingredient__name",
+            "unit__name",
+            "unit__name_plural",
+            "unit__abbr_singular",
+            "unit__abbr_plural",
+        )
+        .annotate(
+            quantity=Sum("quantity"),
+            order_id=Min(F("step__order_id") * 100 + F("order_id")),
+        )
+        .order_by("order_id")
+    ).all()
 
-    return render(request, "recipes/recipe/detail.html", {"recipe": recipe})
+    return render(
+        request,
+        "recipes/recipe/detail.html",
+        {"recipe": recipe, "ingredients": ingredients},
+    )
 
 
 def recipe_create(request): ...
