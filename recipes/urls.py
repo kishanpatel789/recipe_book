121322889e7bcb1ecd3e48142aaf34083e3d4bb0
Kishan Patel <kishanpatel789@gmail.com>
2025-08-26 09:53:50 -0500

    Remove page-based ingredient views

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("recipes/", views.recipe_list, name="recipe_list"),
     path("recipes/<str:recipe_slug>/", views.recipe_detail, name="recipe_detail"),
     path("ingredients/", views.ingredient_list, name="ingredient_list"),
-    path("ingredients/create/", views.ingredient_create, name="ingredient_create"),
     path(
         "htmx/ingredients/create/",
         views.htmx_ingredient_create,
@@ -22,13 +21,5 @@ urlpatterns = [
         "htmx/ingredients/delete/<int:ingr_id>/",
         views.htmx_ingredient_delete,
         name="htmx_ingredient_delete",
-    ),
-    path(
-        "ingredients/edit/<int:ingr_id>/", views.ingredient_edit, name="ingredient_edit"
-    ),
-    path(
-        "ingredients/delete/<int:ingr_id>/",
-        views.ingredient_delete,
-        name="ingredient_delete",
     ),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.db.models import F, Min, Sum
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.http import require_http_methods, require_POST
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_http_methods
 
 from .forms import IngredientCreateForm, IngredientEditForm
 from .models import Ingredient, Recipe, StepIngredient
@@ -52,48 +52,6 @@ def ingredient_list(request):
     ingredients = Ingredient.objects.all()
 
     return render(request, "recipes/ingredient/list.html", {"ingredients": ingredients})
-
-
-def ingredient_create(request):
-    if request.method == "POST":
-        form = IngredientCreateForm(request.POST)
-        if form.is_valid():
-            ingredient_name = form.cleaned_data["name"]
-
-            Ingredient(name=ingredient_name).save()
-            return redirect("ingredient_list")
-
-    else:
-        form = IngredientCreateForm()
-
-    return render(request, "recipes/ingredient/create.html", {"form": form})
-
-
-def ingredient_edit(request, ingr_id):
-    db_ingredient = get_object_or_404(Ingredient, id=ingr_id)
-
-    if request.method == "POST":
-        form = IngredientEditForm(request.POST, instance=db_ingredient)
-        if form.is_valid():
-            db_ingredient.name = form.cleaned_data["name"]
-            db_ingredient.save()
-
-            return redirect("ingredient_list")
-
-    else:
-        form = IngredientEditForm(instance=db_ingredient)
-
-    return render(request, "recipes/ingredient/edit.html", {"form": form})
-
-
-@require_POST
-def ingredient_delete(request, ingr_id):
-    db_ingredient = get_object_or_404(Ingredient, id=ingr_id)
-
-    if request.method == "POST":
-        db_ingredient.delete()
-
-        return redirect("ingredient_list")
 
 
 def htmx_ingredient_create(request):
