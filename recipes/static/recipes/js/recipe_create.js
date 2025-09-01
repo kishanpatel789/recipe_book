2932509e08e090e7553d74015ffea5e96513522a
Kishan Patel <kishanpatel789@gmail.com>
2025-09-01 10:42:42 -0500

    Add data attr and delegate ingr click

--- recipes/static/recipes/js/recipe_create.js
+++ recipes/static/recipes/js/recipe_create.js
@@ -1,29 +1,31 @@
-const totalFormsInput = document.querySelector("#id_step-TOTAL_FORMS");
-const minNumForms = parseInt(document.querySelector("#id_step-MIN_NUM_FORMS").value)
-const maxNumForms = parseInt(document.querySelector("#id_step-MAX_NUM_FORMS").value)
+const totalStepFormsInput = document.querySelector("#id_step-TOTAL_FORMS");
+const minNumStepForms = parseInt(document.querySelector("#id_step-MIN_NUM_FORMS").value)
+const maxNumStepForms = parseInt(document.querySelector("#id_step-MAX_NUM_FORMS").value)
 const stepList = document.querySelector("#step-list");
-const emptyFormTemplate = document.querySelector("#step-template");
+const stepFormTemplate = document.querySelector("#step-template");
 const addStepButton = document.querySelector("#add-step");
 const removeStepButton = document.querySelector("#remove-step");
 
 function addStep() {
-  const formIndex = parseInt(totalFormsInput.value);
-  if (formIndex < maxNumForms) {
-    const newForm = emptyFormTemplate.cloneNode(true);
+  const formIndex = parseInt(totalStepFormsInput.value);
+  if (formIndex < maxNumStepForms) {
+    const newForm = stepFormTemplate.cloneNode(true);
     newForm.removeAttribute("id")
     newForm.style.display = "";  // make visible
     newForm.innerHTML = newForm.innerHTML.replace(/__prefix__/g, formIndex);
+    newForm.querySelectorAll(".add-ingredient, .remove-ingredient")
+      .forEach(btn => btn.setAttribute("data-step-index", formIndex));
 
     stepList.appendChild(newForm);
-    totalFormsInput.value = formIndex + 1;
+    totalStepFormsInput.value = formIndex + 1;
   }
 }
 
 function removeStep() {
-  const formIndex = parseInt(totalFormsInput.value);
-  if (formIndex > minNumForms) {
+  const formIndex = parseInt(totalStepFormsInput.value);
+  if (formIndex > minNumStepForms) {
     stepList.lastElementChild.remove();
-    totalFormsInput.value = formIndex - 1;
+    totalStepFormsInput.value = formIndex - 1;
   }
 }
 
@@ -35,4 +37,21 @@ addStepButton.addEventListener("click", (e) => {
 removeStepButton.addEventListener("click", (e) => {
   e.preventDefault();
   removeStep();
+});
+
+// delegate add/remove ingredient action
+stepList.addEventListener("click", e => {
+  if (e.target.matches(".add-ingredient")) {
+    e.preventDefault();
+    const stepIndex = e.target.getAttribute("data-step-index");
+    console.log(`Add ingredient to step ${stepIndex}`);
+    // TODO: implement add ingredient
+  }
+
+  if (e.target.matches(".remove-ingredient")) {
+    e.preventDefault();
+    const stepIndex = e.target.getAttribute("data-step-index");
+    console.log(`Remove ingredient from step ${stepIndex}`);
+    // TODO: implement remove ingredient
+  }
 });
