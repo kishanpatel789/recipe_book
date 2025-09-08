99c5549031d30e9c6020bb61b22c8fd4b29392eb
Kishan Patel <kishanpatel789@gmail.com>
2025-09-08 11:23:55 -0500

    Extend user model to profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,27 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    @property
+    def is_chef(self):
+        return self.user.groups.filter(name="Chef").exists()
+
+    @property
+    def is_cook(self):
+        return self.user.groups.filter(name="Cook").exists()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
