68a89e0fb082a33dca61ff733c3688db89fcc24a
Kishan Patel <kishanpatel789@gmail.com>
2025-09-15 11:50:09 -0500

    Add STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "recipes/static",
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
